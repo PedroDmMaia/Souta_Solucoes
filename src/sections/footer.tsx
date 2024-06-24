0cb3caa283c9cf3a4e9a81b78af90b0bb3a33115
Pedro <pedrodelmonico@hotmail.com>
2024-06-24 01:02:06 -0300

    finished footer section

--- src/sections/footer.tsx
+++ src/sections/footer.tsx
@@ -1,14 +1,51 @@
-import imageFooter from '../../public/Logo.png'
+import { Download, InstagramLogo, LinkedinLogo } from '@phosphor-icons/react'
 
 export function Footer() {
   return (
     <footer className="bg-cor-fundo md:px-10 xl:px-[21rem] px-7">
-      <div className="flex flex-col text-center gap-4 md:gap-0 md:flex-row md:text-start justify-between items-center py-4">
-        <img src={imageFooter} className="w-60" alt="" />
-        <p className="text-corTexto">
-          &copy; 2024 Souta Soluções - Todos os direitos reservados.
-        </p>
+      <div className="flex flex-col text-center gap-4 md:gap-0 md:flex-row md:text-start justify-between  py-4 text-corTexto">
+        <div className="flex flex-col gap-4">
+          <span className="text-xl font-semibold uppercase">
+            Fale com a gente
+          </span>
+
+          <div className="flex flex-col gap-[0.20rem]">
+            <a>+55 11 3815-3533</a>
+            <p>atendimento@igcp.com.br</p>
+          </div>
+
+          <div className="flex flex-col gap-1">
+            <p className="w-72">
+              Brasil: Av. Brigadeiro Faria Lime, 2277, 6º andar. São Paulo - SP.
+            </p>
+            <p>CEP 01452-000</p>
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-4">
+          <span className="text-xl font-semibold uppercase">Acompanhe</span>
+
+          <div className="flex gap-3">
+            <a href="" target="_blank">
+              <InstagramLogo size={32} />
+            </a>
+            <a href="" target="_blank">
+              <LinkedinLogo size={32} />
+            </a>
+          </div>
+
+          <span className="uppercase font-semibold">
+            Apresentação institucional
+            <a href="" target="_blank">
+              <Download size={32} weight="fill" />
+            </a>
+          </span>
+        </div>
       </div>
     </footer>
   )
 }
+
+// <p className="text-corTexto">
+//   &copy; 2024 Souta Soluções - Todos os direitos reservados.
+// </p>
